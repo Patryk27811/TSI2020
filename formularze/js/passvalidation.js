cb920349d222fa8e6e6ce8780e591dac031c5e10
Patryk27811 <patryk.dobrowolski24@gmail.com>
2022-02-20 22:23:52 +0100

    ww

--- formularze/js/passvalidation.js
+++ formularze/js/passvalidation.js
@@ -1,6 +1,6 @@
 function checkPassword(form) {
     password1 = form.pass.value;
-    password2 = form.pass2.value;
+    password2 = form.pass1.value;
 
     if (password1 != password2) {
         alert("\nWprowadzone hasła się różnią");
