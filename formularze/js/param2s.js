63bcaf2f48c0153b5c535f3e9b41edae29a35061
patryk27811 <patryk.dobrowolski24@gmail.com>
2022-02-21 16:16:39 +0100

    zmiana stylu oraz dodanie ifów z undefind

--- formularze/js/param2s.js
+++ formularze/js/param2s.js
@@ -1,11 +1,44 @@
-let awatar=params.awatar;
 document.getElementById("login").innerHTML = decodeURIComponent(params.login);
 document.getElementById("pass").innerHTML = decodeURIComponent(params.pass);
-document.getElementById("awatar").innerHTML = decodeURIComponent(params.awatar);
-switch(awatar){
-case 1:
-    document.getElementById("awatar").innerHTML = document.write("<img src='/img/awatar1.jpg'/>");
+switch(parseInt(params.awatar)){
+    case 1:
+    document.getElementById("awatar").innerHTML = "<img src='/formularze/img/awatar1.jpg'/>";
+    
+    break;
+    case 2:
+    document.getElementById("awatar").innerHTML = "<img src='/img/awatar2.jpg'/>";
+    break;
+    case 3:
+    document.getElementById("awatar").innerHTML = "<img src='/img/awatar3.jpg'/>";
+    break;
+    case 4:
+    document.getElementById("awatar").innerHTML = "<img src='/img/awatar4.jpg'/>";
+    break;
+    case 5:
+    document.getElementById("awatar").innerHTML = "<img src='/img/awatar5.jpg'/>";
+    break;
+    case 6:
+    document.getElementById("awatar").innerHTML = "<img src='/img/awatar1.jpg'/>";
+    break;
 };
+if(params.kpodszkola1==undefined){
+    params.kpodszkola1="";
+}
+if(params.kpodszkola2==undefined){
+    params.kpodszkola2="";
+}
+if(params.kpodszkola3==undefined){
+    params.kpodszkola3="";
+}
+if(params.ksredniaszkola1==undefined){
+    params.ksredniaszkola1="";
+}
+if(params.ksredniaszkola2==undefined){
+    params.ksredniaszkola2="";
+}
+if(params.ksredniaszkola3==undefined){
+    params.ksredniaszkola3="";
+}
 
 document.getElementById("name").innerHTML = decodeURIComponent(params.name);
 document.getElementById("last_name").innerHTML = decodeURIComponent(params.last_name);
@@ -16,8 +49,8 @@ document.getElementById("email").innerHTML = decodeURIComponent(params.email);
 document.getElementById("wojewodztwa").innerHTML = decodeURIComponent(params.wojewodztwa);
 document.getElementById("city").innerHTML = decodeURIComponent(params.city);
 document.getElementById("ul").innerHTML = decodeURIComponent(params.ul);
-document.getElementById("pcode").innerHTML = decodeURIComponent(params.pcode);
-document.getElementById("nr").innerHTML = decodeURIComponent(params.nr);
+document.getElementById("pcode").innerHTML=decodeURIComponent(params.pcode)+" - "+decodeURIComponent(params.pcode2);
+document.getElementById("nr").innerHTML=decodeURIComponent(params.nr)+" / "+decodeURIComponent(params.nr2);
 document.getElementById("kwls").innerHTML = decodeURIComponent(params.kwls);
 document.getElementById("cocz").innerHTML = decodeURIComponent(params.cocz);
 document.getElementById("plc").innerHTML = decodeURIComponent(params.plc);
